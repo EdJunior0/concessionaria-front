27aa917ae15a2124aa389d41c7b6e8d85fbd5af3
Edson Júnior <edsonjunior.1219@gmail.com>
2022-07-09 21:56:20 -0300

    change api url

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 import apiFactory from "./api";
 
-const apiUrl = "http://localhost:8080";
+const apiUrl = "https://concessionaria-api.herokuapp.com/";
 
 const api = apiFactory({
   api_url: apiUrl,
